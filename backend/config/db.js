407e44ec9aa6930c21d504130630298675cff170
basiljamildev <thdev4.2@gmail.com>
2025-07-21 17:44:41 +0500

    a

--- backend/config/db.js
+++ backend/config/db.js
@@ -21,13 +21,11 @@ if (process.env.DATABASE_URL) {
   );
 }
 
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("✅ Connection has been established successfully.");
-  } catch (error) {
-    console.error("❌ Unable to connect to the database:", error);
-  }
-})();
+try {
+  await sequelize.authenticate();
+  console.log("✅ Connection has been established successfully.");
+} catch (error) {
+  console.error("❌ Unable to connect to the database:", error);
+}
 
 module.exports = sequelize;
